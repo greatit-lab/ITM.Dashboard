a268987ca9c78bf1126daeafac354da4a34373a8
greatit-lab <gil2zum@gmail.com>
2025-09-30 18:14:20 +0900

    Create EquipmentListItemDto.cs -- 0930

---
+++ ITM.Dashboard.Web.Client/Models/EquipmentListItemDto.cs
@@ -0,0 +1,10 @@
+// ITM.Dashboard.Web.Client/Models/EquipmentListItemDto.cs
+namespace ITM.Dashboard.Web.Client.Models
+{
+    public class EquipmentListItemDto
+    {
+        public string EqpId { get; set; }
+        public string PcName { get; set; }
+        public bool IsOnline { get; set; }
+    }
+}
