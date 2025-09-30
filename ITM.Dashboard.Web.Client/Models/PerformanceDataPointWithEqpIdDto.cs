57ee6f3e75912b02247b6ba02e1828d71edf4523
greatit-lab <gil2zum@gmail.com>
2025-10-01 01:10:31 +0900

    Update PerformanceDataPointWithEqpIdDto.cs ++ 1001_01

--- ITM.Dashboard.Web.Client/Models/PerformanceDataPointWithEqpIdDto.cs
+++ ITM.Dashboard.Web.Client/Models/PerformanceDataPointWithEqpIdDto.cs
@@ -9,5 +9,10 @@ namespace ITM.Dashboard.Web.Client.Models
         public DateTime Timestamp { get; set; }
         public double CpuUsage { get; set; }
         public double MemoryUsage { get; set; }
+
+        // ▼▼▼ [추가] API에서 보내주는 새로운 속성들을 정의합니다. ▼▼▼
+        public double CpuTemp { get; set; }
+        public double GpuTemp { get; set; }
+        public double FanSpeed { get; set; }
     }
 }
