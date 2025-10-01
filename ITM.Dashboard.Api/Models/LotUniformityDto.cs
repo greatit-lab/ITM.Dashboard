28d7b294f94e8ebb3d5d37ba8b6c8ce019d6e7a3
greatit-lab <gil2zum@gmail.com>
2025-10-01 13:00:47 +0900

    Update LotUniformityDto.cs -- 1001_13

--- ITM.Dashboard.Api/Models/LotUniformityDto.cs
+++ ITM.Dashboard.Api/Models/LotUniformityDto.cs
@@ -5,15 +5,13 @@ namespace ITM.Dashboard.Api.Models
 {
     public class LotUniformityDataPointDto
     {
-        public int Point { get; set; } 
+        public int Point { get; set; }
         public double Value { get; set; }
         public double X { get; set; }
         public double Y { get; set; }
 
-        // ▼▼▼ [수정] int?를 double?로 변경합니다. ▼▼▼
         public double? DieRow { get; set; }
         public double? DieCol { get; set; }
-        // ▲▲▲ [수정] 여기까지 ▲▲▲
     }
 
     public class LotUniformitySeriesDto
